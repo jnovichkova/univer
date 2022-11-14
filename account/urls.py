c6c5cfd0cc0d8b8c81dd6172d75bbd116c3fe662
jnovichkova <julia.novichkova@icloud.com>
2022-11-14 16:29:10 +0200

    Add views && routes for account

--- account/urls.py
+++ account/urls.py
@@ -3,5 +3,10 @@ from .views import *
 
 
 urlpatterns = [
-
+    path('reg', reg),
+    path('entry', entry),
+    path('confirm', confirm),
+    path('exit', exit),
+    path('profile', profile),
+    path('reset', reset),
 ]
